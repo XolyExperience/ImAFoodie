23cc4d26270c2149826507a6391739a70d69eb52
Xoly <Beff.Henriksson@live.se>
2021-03-17 11:32:38 +0100

    layout fixes and css + fixed js function

--- js/main.js
+++ js/main.js
@@ -1,15 +1,19 @@
 // Variables
+const menuRow = document.querySelector('.menuRow');
 const menuBtn = document.querySelector('.menu-btn');
-const menu = document.querySelector('.menu');
-let menuOpen = false;
-var errorText = document.getElementById('errorText');
-let userLat = 40.71;
-let userLong = -74.0;
-let userDist = 500;
-const endpoint = `https://api.documenu.com/v2/restaurants/search/geo?lat=${userLat}&lon=${userLong}&distance=${userDist}`;
+const sortBtn = document.querySelector('.sort-btn');
 
-// MainCode
-doFetch();
+const menu = document.querySelector('.menu');
+const sortMenu = document.querySelector('.menu-sort');
+
+let menuOpen = false;
+let sortMenuOpen = false;
+
+var errorText = document.getElementById('errorText');
+var userLat = 40.71;
+var userLong = -74.0;
+var userDist = document.getElementById('distance').value;
+const APIURL = `https://api.documenu.com/v2/restaurants/search/geo`;
 
 // Functions
 function getLocation() {
@@ -19,20 +23,18 @@ function getLocation() {
         errorText.innerHTML = 'Geolocation is not supported by this browser.';
     }
 }
-
 function addPosition(position) {
     userLat = position.coords.latitude.toFixed(6);
     userLong = position.coords.longitude.toFixed(6);
 }
+function sort() {
+    getRestaurants(userLat, userLong, userDist, APIURL);
 
-function sorter() {
-    userDist = document.getElementById('distance').value;
-    doFetch();
 }
-
-function doFetch() {
+function getRestaurants(lat, long, dist, url) {
+    let endpoint = url += `?lat=${lat}&lon=${long}&distance=${dist}`;
     // Fetch data from the API
-    fetch(endpoint, {
+    fetch(`${endpoint}`, {
             method: 'GET',
             headers: {
                 'X-API-KEY': 'c51a4f7b9a4fbf37731d16fd26245c73',
@@ -40,17 +42,21 @@ function doFetch() {
         })
         .then(response => response.json())
         .then(restaurants => {
-            restaurants.forEach(restaurant => {
+            restaurants.data.forEach(restaurant => {
+                // Create card element
+                const cardElement = document.createElement("div");
                 // Create title element
                 const titleElement = document.createElement("h3");
-                titleElement.textContent = data.data[1].restaurant_name;
+                titleElement.textContent = restaurant.restaurant_name;
                 // Create content element
                 const contentElement = document.createElement("p");
-                contentElement.textContent = restaurant.content;
+                contentElement.textContent = restaurant.restaurant_phone;
                 // Add title and content to the page
                 const restaurantsElement = document.getElementById("restaurants");
-                restaurantsElement.appendChild(titleElement);
-                restaurantsElement.appendChild(contentElement);
+                restaurantsElement.appendChild(cardElement);
+                cardElement.classList.add("card");
+                cardElement.appendChild(titleElement);
+                cardElement.appendChild(contentElement);
             });
         })
         .catch(err => {
@@ -58,15 +64,30 @@ function doFetch() {
         });
 }
 
+// Main
+getRestaurants(userLat, userLong, userDist, APIURL);
+
 // EventListeners
 menuBtn.addEventListener('click', () => {
     if (!menuOpen) {
-        menuBtn.classList.add('open');
+        menuRow.classList.add('open');
         menu.classList.add('open');
         menuOpen = true;
     } else {
-        menuBtn.classList.remove('open');
+        menuRow.classList.remove('open');
         menu.classList.remove('open');
         menuOpen = false;
+    }
+});
+
+sortBtn.addEventListener('click', () => {
+    if (!sortMenuOpen) {
+        sortMenu.classList.add('open');
+        menuRow.classList.add('open');
+        sortMenuOpen = true;
+    } else {
+        sortMenu.classList.remove('open');
+        menuRow.classList.remove('open');
+        sortMenuOpen = false;
     }
 });
